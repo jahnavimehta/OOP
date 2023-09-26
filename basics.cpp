2095f283bc6aab02fd9fba2b9853bc5fc01ec27e
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-09-26 21:52:51 +0530

    Create basics.cpp

---
+++ basics.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Hero{
+  //properties
+  int health;
+}
+
+int main(){
+  //creation of object
+  Hero h1;
+  cout << "size: " << sizeOf(h1) << endl; //prints 4 since size of all properties rn is integer is 4 bytes
+  
+  
+}

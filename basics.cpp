2754dd51acc3f2598de240dd5141ffcacf890eeb
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-09-27 00:07:05 +0530

    Update basics.cpp

--- basics.cpp
+++ basics.cpp
@@ -9,7 +9,5 @@ class Hero{
 int main(){
   //creation of object
   Hero h1;
-  cout << "size: " << sizeOf(h1) << endl; //prints 4 since size of all properties rn is integer is 4 bytes
-  
-  
+  cout << "size: " << sizeOf(h1) << endl; //prints 4 since size of all properties rn is integer is 4 bytes  
 }

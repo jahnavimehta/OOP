128cfa81bbdf724d20e6e49ad88599d6fd09cbf4
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-10-07 22:45:54 +0530

    Create k) private-protected.cpp

---
+++ lec2/k) private-protected.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namesace std;
+
+class Human{
+  public:
+    int height;
+    int weight;
+  private:
+    int age;
+
+  public:
+    int getAge(){
+      return this -> age;
+    }
+
+    void setWeight(int w){
+      this->weight = w;
+    }
+};
+
+class Male: protected Human{
+  public:
+    string color;
+
+    void sleep(){
+      cout << "Male Sleeping" << endl;
+    }
+
+    int getHeight() {
+       return this->height; //INACCESSIBLE even inside class
+    }
+};
+
+int main(){
+  Male m1;
+  cout << m1.height << endl;   //INACESSIBLE
+  
+  return 0;
+}

56cd7b6ea74c464e34a19c3a83d2e102d63cb4c4
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-10-07 15:41:49 +0530

    Create k)parameterisedConstructor.cpp

---
+++ Lec1/k)parameterisedConstructor.cpp
@@ -0,0 +1,48 @@
+class Hero{
+  private:
+  int health;
+
+  public:
+  char level;
+
+  //default constructor
+  Hero(){
+    cout<< "constructor called" <<endl;
+
+  //parameterised contructor
+  Hero(int health){
+   cout<< "this ->" << this << endl; //marker...
+   this->health = health;
+  }
+
+  void print(){
+    cout<< level<< endl;
+  }
+
+  int getHealth(){
+    return health;
+  }
+
+  char getLevel(){
+    return level;
+  }
+
+  void setHealth(int h){ 
+      health = h;
+  }
+
+  void setLevel(char ch){
+    level = ch;
+  }  
+}
+
+int main(){
+  //object created statically
+  Hero ramesh(10); //param constructor called
+  cout<< "address of ramesh" << &ramesh <<endl;  //marker...
+  ramesh.getHealth();
+  
+
+  //object created dynamically
+  Hero *h = new Hero;  // default constructor called
+}

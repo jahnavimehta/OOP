d9c57df55ef1b42bed6e3e7a93e8721bd04197f1
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-10-07 13:33:57 +0530

    Create accessPrivateMember.cpp

---
+++ Lec1/accessPrivateMember.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Hero{
+  int health;
+  char level;
+}
+
+int main(){
+  Hero h1;
+  cout << "health is: " << ramesh.health << endl; //error
+  cout << "level is: " << ramesh.level << endl;  //error
+}

2eec2c61f167526b3b7011e120d6d86e3a91adbf
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-10-07 13:30:09 +0530

    classUsingHeaderFile.cpp

---
+++ Lec1/class in some other file.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include "Hero.cpp" //include header file of file where class stored
+using namespace std;
+
+int main(){
+  Hero h1;
+  cout << "size: " << sizeOf(h1) << endl;
+}
+/*****************************************/
+//other file
+class Hero{
+  int health;
+}

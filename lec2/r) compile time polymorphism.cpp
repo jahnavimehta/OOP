f5cc7b359eb2802d91f377d85ad4e78ce87efa14
Jahnavi Mehta <97538596+jahnavimehta@users.noreply.github.com>
2023-10-07 23:39:04 +0530

    Update r) compile time polymorphism.cpp

--- lec2/r) compile time polymorphism.cpp
+++ lec2/r) compile time polymorphism.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+using namesace std;
 
+class A {
+   public:
+   void sayHello(){
+     cout << "Hello Jahnavi" << endl;
+   }
+
+   //number of input arguments changed
+   int sayHello(string name, int n){
+      cout << "Hello Jahnavi" << endl;
+      return n;
+   }
+
+   //input argument datatype changed
+   void sayHello(string name) {
+     cout << "Hello" << name << endl;
+   }
+};
+
+int main(){    
+  A obj;
+  obj.sayHello();
+  
+  return 0;
+}
